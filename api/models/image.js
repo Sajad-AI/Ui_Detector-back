56950bf67c71d443417baed9da98d7cf0fd695c6
sim <sajad@simsysglobal.com>
2021-12-19 22:37:46 +0900

    removed id and price

--- api/models/image.js
+++ api/models/image.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 const imageSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
-    price: { type: Number, required: true },
     productImage: { type: String, required: true }
 });
 
--- api/routes/images.js
+++ api/routes/images.js
@@ -31,47 +31,45 @@ const upload = multer({
 
 const Product = require("../models/image");
 
-router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id productImage")
-    .exec()
-    .then(docs => {
-      const response = {
-        count: docs.length,
-        products: docs.map(doc => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id
-            }
-          };
-        })
-      };
-      //   if (docs.length >= 0) {
-      res.status(200).json(response);
-      //   } else {
-      //       res.status(404).json({
-      //           message: 'No entries found'
-      //       });
-      //   }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+// router.get("/", (req, res, next) => {
+//   Product.find()
+//     .select("name price _id productImage")
+//     .exec()
+//     .then(docs => {
+//       const response = {
+//         count: docs.length,
+//         products: docs.map(doc => {
+//           return {
+//             name: doc.name,
+//             price: doc.price,
+//             productImage: doc.productImage,
+//             _id: doc._id,
+//             request: {
+//               type: "GET",
+//               url: "http://localhost:3000/products/" + doc._id
+//             }
+//           };
+//         })
+//       };
+//       //   if (docs.length >= 0) {
+//       res.status(200).json(response);
+//       //   } else {
+//       //       res.status(404).json({
+//       //           message: 'No entries found'
+//       //       });
+//       //   }
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json({
+//         error: err
+//       });
+//     });
+// });
 
 router.post("/", upload.single('productImage'), (req, res, next) => {
   const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price,
     productImage: req.file.path 
   });
   product
@@ -82,8 +80,6 @@ router.post("/", upload.single('productImage'), (req, res, next) => {
         message: "Created product successfully",
         createdProduct: {
             name: result.name,
-            price: result.price,
-            _id: result._id,
             request: {
                 type: 'GET',
                 url: "http://localhost:3000/products/" + result._id
