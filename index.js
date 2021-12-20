69e14b3a93cde15fbf588af6f5167c343c50477d
sim <sajad@simsysglobal.com>
2021-12-20 13:38:31 +0900

    updated

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-var express = require('express')
-var multer  = require('multer')
+var express = require("express");
+var multer = require("multer");
 var port = 3000;
 
-var app = express()
+var app = express();
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-})
-var upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date() + "_" + file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
 /*
 app.use('/a',express.static('/b'));
 Above line would serve all files/folders inside of the 'b' directory
 And make them accessible through http://localhost:3000/a.
 */
-app.use(express.static(__dirname + '/public'));
-app.use('./uploads', express.static('uploads'));
+app.use(express.static(__dirname + "/public"));
+app.use("./uploads", express.static("uploads"));
 
-app.post('/imageUpload', upload.single('profile-file'), function (req, res, next) {
-  // req.file is the `profile-file` file
-  // req.body will hold the text fields, if there were any
-  console.log(JSON.stringify(req.file))
-  var response = '<a href="/">Home</a><br>'
-  response += "Files uploaded successfully.<br>"
-  response += `<img src="${req.file.path}" /><br>`
-  return res.send(response)
-})
+app.post(
+  "/imageUpload",
+  upload.single("image-file"),
+  function (req, res, next) {
+    // req.file is the `profile-file` file
+    // req.body will hold the text fields, if there were any
+    console.log(JSON.stringify(req.file));
+    var response = {
+      status: "Image uploaded successfully.",
+      imagePath: req.file.path,
+    };
+    // var response = '<a href="/">Home</a><br>'
+    // response += "Files uploaded successfully.<br>"
+    // response += `<img src="${req.file.path}" /><br>`
+    return res.send(response);
+  }
+);
 
-app.listen(port,() => console.log(`Server running on port ${port}!\nClick http://localhost:3000/`))
+app.listen(port, () =>
+  console.log(`Server running on port ${port}!\nClick http://localhost:3000/`)
+);
