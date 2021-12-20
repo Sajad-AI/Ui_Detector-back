740df51db91036a17bf498ae2f9cdc7a18f2b634
sim <sajad@simsysglobal.com>
2021-12-20 13:25:48 +0900

    new code, working fine

--- api/models/image.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const imageSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    productImage: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Image', imageSchema);
