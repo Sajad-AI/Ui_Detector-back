b00ec93497fb1525d686e44a254d230fec1ec22e
sim <sajad@simsysglobal.com>
2021-12-27 14:09:40 +0900

    user can download images from uploads folder

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ var storage = multer.diskStorage({
     cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + "_" + file.originalname.replace(/ /g,"_"));
+    cb(null, new Date().toISOString() + "_" + file.originalname.replace(/ /g, "_"));
   },
 });
 var upload = multer({ storage: storage });
 
 app.use(express.static(__dirname + "/public"));
-app.use("./uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
-app.post(
-  "/imageUpload",
-  upload.single("image-file"),
-  function (req, res, next) {
-    console.log(JSON.stringify(req.file));
-    var response = {
-      status: "Image uploaded successfully.",
-      imagePath: req.file.path,
-    };
-    return res.send(response);
-  }
+app.post("/imageUpload", upload.single("image-file"), function (req, res, next) {
+  console.log(JSON.stringify(req.file));
+  var response = {
+    status: "Image uploaded successfully.",
+    imagePath: req.file.path,
+  };
+  return res.send(response);
+}
 );
 
+
 app.listen(port, () =>
-  console.log('Server running on port ${port}!')
+  console.log('http://localhost:8080. Server running on port: ' + port)
 );
