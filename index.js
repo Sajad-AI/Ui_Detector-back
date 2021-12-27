3b110af2a70800c3b9b6c5216f82266ee18dc806
sim <sajad@simsysglobal.com>
2021-12-27 14:34:06 +0900

    imageUpload -> fixed imagePath link to downloadable link

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var multer = require("multer");
+const path = require('path');
+
 var app = express();
 
 const port = process.env.PORT || 8080;
@@ -17,17 +19,21 @@ var upload = multer({ storage: storage });
 app.use(express.static(__dirname + "/public"));
 app.use("/uploads", express.static("uploads"));
 
+// imageUpload API
 app.post("/imageUpload", upload.single("image-file"), function (req, res, next) {
   console.log(JSON.stringify(req.file));
+  console.log("req.url " + req.url);
+  console.log("req.hostname " + req.hostname);
+  console.log("req.headers.host " + req.headers.host);
+
   var response = {
     status: "Image uploaded successfully.",
-    imagePath: req.file.path,
+    imagePath: "http://" + req.headers.host + "/" + req.file.path,
   };
   return res.send(response);
 }
 );
 
-
-app.listen(port, () =>
+app.listen(port, () => {
   console.log('http://localhost:8080. Server running on port: ' + port)
-);
+});
